e2e8d7832f60f69f08b8af095f9d3f1d7d3e7d38
YZDing-ShanghaiCN <dyz18975197889@gmail.com>
2026-03-25 19:49:17 +0800

    auto-commit

--- test_demo.py
+++ test_demo.py
@@ -40,7 +40,7 @@ def main():
     glctx = dr.RasterizeCudaContext()
     n = bboxes.shape[0]
 
-    for i in range(n-1, n):
+    for i in range(1):
         ob_mask = ob_masks[i]
         bbox = bboxes[i]
         vis = rgb.copy()
@@ -59,47 +59,38 @@ def main():
             poisson_mesh.remove_degenerate_triangles()
             poisson_mesh.remove_duplicated_triangles()
             poisson_mesh.remove_duplicated_vertices()
-            poisson_mesh.remove_non_manifold_edges()    
+            poisson_mesh.remove_non_manifold_edges()
+            poisson_mesh = poisson_mesh.simplify_quadric_decimation(target_number_of_triangles=15000)
             
             vertices = np.asarray(poisson_mesh.vertices)
             center = vertices.mean(axis=0)
             vertices -= center
             mesh = trimesh.Trimesh(vertices=vertices, faces=np.asarray(poisson_mesh.triangles), vertex_normals=np.asarray(poisson_mesh.vertex_normals), process=True)
             print(f"\nMesh reconstructed successfully! Offset from camera: {center}\n")
+            mesh.vertices += center
+            mesh.merge_vertices()
+            mesh.update_faces(mesh.nondegenerate_faces())
+            mesh.update_faces(mesh.unique_faces())
 
         else:
-            # mesh文件是ply格式
-            mesh_path = "./pre_result/crate/crate_1_visual.ply"
+            mesh_path = "./pre_result/crate/crate_0_visual.ply"
             mesh = trimesh.load(mesh_path, process=True)
             mesh.update_faces(mesh.nondegenerate_faces())
             mesh.update_faces(mesh.unique_faces())
             mesh.merge_vertices()
 
-            mask_valid = (ob_mask > 0) & (depth > 1e-6)
-            depth_obj = depth[mask_valid]
-
-            print("min:", depth_obj.min())
-            print("max:", depth_obj.max())
-            print("mean:", depth_obj.mean())
-            print("median:", np.median(depth_obj))
-            print("std:", depth_obj.std())
-
-            sys.exit(0)
-
-            # ## 显示点云 从depth和mask生成
-            # pcd = o3d.geometry.PointCloud()
+            # # 根据mask和depth 可视化点云
             # xyz_map = depth2xyzmap(depth, K_orig)
             # valid = (depth > 0.001) & ob_mask
+            # pcd = o3d.geometry.PointCloud()
             # pcd.points = o3d.utility.Vector3dVector(xyz_map[valid])
             # pcd.colors = o3d.utility.Vector3dVector(rgb[valid] / 255.0)
+            # pcd.estimate_normals(search_param=o3d.geometry.KDTreeSearchParamKNN(knn=50))
+            # pcd.orient_normals_towards_camera_location(camera_location=np.array([0., 0., 0.]))
+            # print(f"\nMesh loaded successfully! Point cloud visualized with {len(pcd.points)} points.\n")
             # o3d.visualization.draw_geometries([pcd], window_name="Input Point Cloud", width=800, height=600)
-            # # 估计点云的尺寸
-            # bbox_3d = np.array([mesh.vertices.min(axis=0), mesh.vertices.max(axis=0)])
-            # print(f"\nMesh loaded successfully! Estimated object size: {bbox_3d[1] - bbox_3d[0]}\n")
             # sys.exit(0)
 
-        # 加载 foundationpose 模型
-        depth = depth * 2.5
         est = FoundationPose(
             model_pts=mesh.vertices,
             model_normals=mesh.vertex_normals,
@@ -112,56 +103,42 @@ def main():
         )
         print("\nFoundationPose initialized successfully!\n")
 
-        # pose estimation
         pose = est.register(K=K_orig, rgb=vis, depth=depth, ob_mask=ob_mask, iteration=5)
         print(f"\nPose estimation completed! Estimated pose:\n{pose}\n")
-
         bbox_3d = np.array([mesh.vertices.min(axis=0), mesh.vertices.max(axis=0)])
-
-        def draw_box(img, corners_2d):
-            edges = [
-                (0,1),(1,2),(2,3),(3,0),  # bottom
-                (4,5),(5,6),(6,7),(7,4),  # top
-                (0,4),(1,5),(2,6),(3,7)   # vertical
-            ]
-
-            for i,j in edges:
-                p1 = tuple(corners_2d[i].astype(int))
-                p2 = tuple(corners_2d[j].astype(int))
-                cv2.line(img, p1, p2, (0,255,0), 2)
-
-            return img
-        
-        def get_3d_box_corners(bbox):
-            min_pt, max_pt = bbox
-            x0, y0, z0 = min_pt
-            x1, y1, z1 = max_pt
-
-            corners = np.array([
-                [x0, y0, z0],
-                [x1, y0, z0],
-                [x1, y1, z0],
-                [x0, y1, z0],
-                [x0, y0, z1],
-                [x1, y0, z1],
-                [x1, y1, z1],
-                [x0, y1, z1],
-            ])
-            return corners
-
-        # vis = draw_posed_3d_box(K_orig, img=vis, ob_in_cam=pose, bbox=bbox_3d)
+        vis = draw_posed_3d_box(K_orig, img=vis, ob_in_cam=pose, bbox=bbox_3d)
         vis = draw_xyz_axis(vis, ob_in_cam=pose, scale=0.1, K=K_orig, thickness=3, transparency=0, is_input_rgb=True)
-
-        corners = get_3d_box_corners(bbox_3d)
-        corners_cam = (pose[:3,:3] @ corners.T + pose[:3,3:4]).T
-        proj = (K_orig @ corners_cam.T).T
-        proj = proj[:, :2] / proj[:, 2:3]
-        vis = draw_box(vis, proj) 
 
         cv2.imshow('Estimated Pose', vis[...,::-1])
         cv2.imwrite(os.path.join(save_dir, f"result_{i:02d}.png"), vis)
         cv2.waitKey(0)
         cv2.destroyAllWindows()
+
+        # save 3D visualization of the final pose in point cloud with xyzaxis
+        print("="*50)
+        pcd = o3d.geometry.PointCloud()
+        xyz_map = depth2xyzmap(depth, K_orig)
+        valid = (depth > 0.001) & ob_mask
+        pcd.points = o3d.utility.Vector3dVector(xyz_map[valid])
+        pcd.colors = o3d.utility.Vector3dVector(rgb[valid] / 255.0)
+        pcd.estimate_normals(search_param=o3d.geometry.KDTreeSearchParamKNN(knn=50))
+        pcd.orient_normals_towards_camera_location(camera_location=np.array([0., 0., 0.]))
+        ob_in_cam = pose
+        axis_length = 0.1
+        axis_points = np.array([[0, 0, 0], [axis_length, 0, 0], [0, axis_length, 0], [0, 0, axis_length]])
+        
+        # 将坐标轴原点设置为点云的中心，并应用pose的旋转
+        c = np.mean(np.asarray(pcd.points), axis=0)
+        axis_points_transformed = c + (ob_in_cam[:3, :3] @ axis_points.T).T
+        
+        axis_lines = [[0, 1], [0, 2], [0, 3]]
+        axis_colors = [[1, 0, 0], [0, 1, 0], [0, 0, 1]]
+        axis_line_set = o3d.geometry.LineSet(
+            points=o3d.utility.Vector3dVector(axis_points_transformed),
+            lines=o3d.utility.Vector2iVector(axis_lines),
+        )
+        axis_line_set.colors = o3d.utility.Vector3dVector(axis_colors)
+        o3d.visualization.draw_geometries([pcd, axis_line_set], window_name="Final Pose Visualization", width=800, height=600)
 
 if __name__ == "__main__":
     main()
